b75d9bf77d461532cc33182f4a452bdc4098284a
Steven Speek <slspeek@gmail.com>
2013-03-28 15:12:10 +0100

    two more icons, color application later

--- see.sh
+++ see.sh
@@ -5,7 +5,11 @@ for ICON in left_arrow\
             right_arrow\
             up_arrow\
             down_arrow\
-            right_triangle 
+            right_triangle\
+	    stop\
+	    end\
+	    fast_forward\
+	    fast_backward		 
         do
             src/Main --select=$ICON -o $ICON.svg -w 800 -h 800
         done
--- src/Main.hs
+++ src/Main.hs
@@ -2,35 +2,36 @@
  
  import Diagrams.Prelude
  import Diagrams.Backend.SVG.CmdLine
- --import Diagrams.Backend.Cairo.CmdLine
  
- themeFc = aqua
+ themeFc = blue
  
- rightTriangle size color = eqTriangle 1 # rotateBy (3/4)
-                                         # fc color
-                                         # lc color
+ rightTriangle size = eqTriangle 1 # rotateBy (3/4)                                      
                                          
- arrow size color =  rect size (size/3) # fc color 
-                                        # lc color  
-                                     ||| 
-                     rightTriangle (size * (2/3)) color 
+ arrow size =  rect size (size/3) ||| rightTriangle (size * (2/3)) 
  
- rightArrow = arrow 1 themeFc
- leftArrow = arrow 1 themeFc # reflectX
+ rightArrow = arrow 1 
+ leftArrow = rightArrow # reflectX
  upArrow = rightArrow # rotateBy (1/4)
  downArrow = upArrow # rotateBy (1/2) 
- play = rightTriangle 1 themeFc
- 
+ play = rightTriangle 1  
+ block = unitSquare # pad 1.2
+ endIcon = rightArrow ||| (vrule 1 # lw 0.2)
+ fastForward = rightTriangle 1 ||| rightTriangle 1
+ fastBackward = fastForward # reflectX
  
  allIcons = [ ( "right_arrow", rightArrow),
               ( "left_arrow", leftArrow),
               ( "up_arrow", upArrow),
               ( "down_arrow", downArrow),
-              ("right_triangle", play) ]
- 
-  
- 
+              ( "right_triangle", play),
+              ( "stop", block),
+              ( "end", endIcon),
+              ( "fast_forward", fastForward),
+              ( "fast_backward", fastBackward) ]
+
  allIconsArg = map (padIcon 1.1) allIcons
-        where padIcon  x (name, icon)  = (name, icon # pad x) 
+        where padIcon  x (name, icon)  = (name, icon # pad x
+                                                     # fc themeFc
+                                                     # lc themeFc   ) 
  
  main = multiMain allIconsArg
