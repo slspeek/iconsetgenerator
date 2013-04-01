273dfe9a0c14ecec4673c06628f5d900d878ae17
Steven L. Speek <slspeek@gmail.com>
2013-04-02 00:44:36 +0200

    Zoom in and out, and all three colors are arguments of see.sh

--- see.sh
+++ see.sh
@@ -1,4 +1,19 @@
 set -e 
+#set -x
+FC=${1:-"#7F525D"}
+BC=${2:-"#737CA1"}
+LC=${3:-"#C9C299"}
+
+function callMain () {
+	WIDTH=$1
+	HEIGHT=$2
+	FILL=$3
+	BG=$4
+	LINE=$5
+	ICON=$6
+	echo -e "$FILL\n$BG\n$LINE"|src/Main --select=$ICON -o icons/$ICON.png -w $WIDTH -h $HEIGHT 
+}
+
 mkdir -p icons
 PATH=$HOME/tools/bin:$PATH
 ghc --make src/Main.hs
@@ -9,7 +24,7 @@ for ICON in left_arrow\
             right_triangle\
 	        stop\
     	    fast_forward\
-	        fast_backward\
+	        rewind\
             next\
             previous\
             home\
@@ -19,9 +34,11 @@ for ICON in left_arrow\
             mail\
             plus\
             favorite\
+            zoom_in\
+            zoom_out\
             minus        
 do
- src/Main --select=$ICON -o icons/$ICON.png -w 800 -h 800 <<< "#809070";
+ callMain 100 100 $FC $BC $LC $ICON
 done
-src/Main --select=overview -o icons/overview.png -w $(( 18 * 48 ))  -h 48 <<< "#809070"
-eog icons/overview.png
+ callMain $((48 * 20)) 48  $FC $BC $LC overview
+eog icons/zoom_in.png
--- src/Main.hs
+++ src/Main.hs
@@ -4,18 +4,18 @@
  import Diagrams.Backend.Cairo.CmdLine
  import Data.Colour.SRGB
  import Diagrams.Core.Points
+ import Data.Colour (withOpacity)
  
- --themeFc =  sRGB24read "#F88017"
- themeFc =  white
- themeBc =  sRGB24read "#585858"
- themeLc =  sRGB24read "#585858"
- 
+  
  
  favorite =  stroke (star (StarSkip 2) (regPoly 5 1))
  
- plusIcon = stroke ( fromVertices plusPath) # scale (8/9)
+ crossy a =  stroke ( fromVertices plusPath) # scale (8/9)
         where  plusPath =  concat . take 4 . iterate (rotateBy (-1/4)) $ spike 
-               spike = map p2 [(-1/4, 1/4), (-1/4,3/4), (1/4, 3/4), (1/4, 1/4)]
+               b = 1 - a 
+               spike = map p2 [(-a, a), (-a,b), (a, b), (a, a)]
+ 
+ plusIcon = crossy (1/5) 
          
  minusIcon = rect (3/2) (1/2) # scale (3/4)  
  rightTriangle size = eqTriangle 1 # rotateBy (3/4)                                      
@@ -32,8 +32,9 @@
           where a = 1 /6
                 eqSide =  2 * sqrt ( 1/3 *a) 
  
- enveloppe =  mconcat $ map stroke pathList    # lineJoin LineJoinRound 
+ enveloppe themeFc themeLc =  mconcat $ map stroke pathList    # lineJoin LineJoinRound 
                                                # lc themeLc
+                                               # fc themeFc
                                                # centerXY
                                                # lw 0.03
                                                # scale 1.2
@@ -54,7 +55,7 @@
               pathList = [ flap, lowerFlap, env]
               
   
- strutedVrule = strutX 0.1 ||| vrule 1 # lw 0.2 ||| strutX 0.1        
+ strutedVrule = minusIcon # rotateBy (1/4)
         
  rightArrow = leftArrow # reflectX 
  upArrow = rightArrow # rotateBy (1/4)
@@ -70,6 +71,34 @@
  home = endIcon # reflectX
  fastBackward = fastForward # reflectX
  
+ 
+ innerCircle =  circle 0.7
+ 
+ ring :: Path R2
+ ring = circle 1 <> innerCircle
+
+ coloredRing themeFc = stroke ring # fc themeFc # fillRule EvenOdd # lw 0
+ 
+ 
+ loop themeFc themeLc =  innerCircle # lc themeLc <> (arcPath <> (handlePath # fc themeFc))  <> coloredRing themeFc                      
+        
+        where a  = 1/40
+              arcPath = arc (a :: CircleFrac) (-a :: CircleFrac)
+              allArcPoints = concat (pathVertices arcPath)
+              endP = head allArcPoints  
+              startP = last allArcPoints
+              d = 1.5
+              handleTop = endP # translateX d 
+              handleBottom = startP # translateX d 
+              handlePath = fromVertices [endP, handleTop, handleBottom, startP]
+              transparantColor = blue `withOpacity` 0
+ 
+ combineWithLoop diagram themeFc themeLc = ((diagram # fc themeFc # rotateBy (3/8:: CircleFrac) # scale 0.8  <>  loop themeFc themeLc) # rotateBy (-3/8:: CircleFrac) # lc themeLc)# centerXY # scale 0.5 
+              
+ zoomIn  =  (combineWithLoop  plusIcon)
+ 
+ zoomOut  = combineWithLoop minusIcon
+ 
  allIcons = [ ( "right_arrow", rightArrow),
               ( "left_arrow", leftArrow),
               ( "up_arrow", upArrow),
@@ -82,29 +111,44 @@
               ( "stop", block),
               ( "home", home),
               ( "end", endIcon),
-              ( "mail", enveloppe),
+              
               ( "plus", plusIcon),
               ( "minus", minusIcon),
               ( "favorite", favorite),
               ( "fast_forward", fastForward),
-              ( "fast_backward", fastBackward) ]
+              ( "rewind", fastBackward) ]
  
- backgroundShape = regPoly 6 1 # fc themeBc # scale 1.3
- backgroundShape' = circle 1 # fc themeBc
+ hardToColor themeFc themeLc = [( "zoom_out", zoomOut themeFc themeLc),
+                                ( "mail", enveloppe themeFc themeLc),
+                                ( "zoom_in", zoomIn themeFc themeLc) ] 
  
- allIconsArg = map (padIcon 1.1) allIcons
-        where padIcon  x (name, icon)  = (name, (icon  
-                                         <>  backgroundShape') # pad x  ) 
- iconList = map snd allIconsArg
- overviewImage = hcat iconList
+ -- backgroundShape = (star (StarSkip 2) (regPoly 8 1) ) # stroke # fc themeBc # scale 1.3
+ backgroundShape' color lineC = circle 1 # fc color # lc lineC
  
+ setOnBackground bg lineC = map (padIcon 1.1)
+        where padIcon  x (name, icon)  = (name , (icon <> backgroundShape' bg lineC) # lw 0.01 # pad x  )
+         
   
- allIconsFinal = ( "overview", overviewImage) : allIconsArg
+ setColorOnAll fillColor lineColor = map  f  
+        where f ( x, y ) = (x, y # lc lineColor # fc fillColor)
  
- setColorOnAll color  = map  f  
-        where f ( x, y ) = (x, y # fc color # lc color)
  
+ prepareAll fc bg lc =  ( "overview", overviewImage) : allPadded        
+                
+        where   fcC = sRGB24read fc
+                bgC = sRGB24read bg
+                lcC = sRGB24read lc 
+                simpleIcons = setColorOnAll fcC lcC allIcons
+                hardIcons = hardToColor fcC lcC
+                allList = concat [simpleIcons, hardIcons]
+                allPadded = setOnBackground bgC lcC allList
+                iconList = map snd allPadded
+                overviewImage = hcat iconList
  
  main = do
-       colorString <- getLine
-       multiMain $  setColorOnAll (sRGB24read colorString) allIconsFinal 
+       fcString <- getLine
+       bgString <- getLine
+       lcString <- getLine
+       print fcString 
+       print lcString
+       multiMain  (prepareAll fcString bgString lcString) 
