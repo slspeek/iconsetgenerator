7d045a2465a60009aa4d16a99d2bbc6cfa2961d5
Steven Speek <slspeek@gmail.com>
2013-04-04 18:53:23 +0200

    call it a day

--- install_haskell.sh
+++ install_haskell.sh
@@ -1,2 +1,7 @@
-wget http://lambda.haskell.org/platform/download/2012.4.0.0/haskell-platform-2012.4.0.0.tar.gz
-wget http://www.haskell.org/ghc/dist/7.4.2/ghc-7.4.2-x86_64-unknown-linux.tar.bz2
+cd ~
+HASKELL_PLATFORM=haskell-platform-2012.4.0.0.tar.gz
+wget http://lambda.haskell.org/platform/download/2012.4.0.0/$HASKELL_PLATFORM
+tar xvzf $HASKELL_PLATFORM
+GHC=ghc-7.4.2-x86_64-unknown-linux.tar.bz2
+wget http://www.haskell.org/ghc/dist/7.4.2/$GHC
+tar xvjf $GHC
---
+++ run_as_root.sh
@@ -0,0 +1,2 @@
+apt-get update
+apt-get install bzip2 build-essential libgmp3-dev
