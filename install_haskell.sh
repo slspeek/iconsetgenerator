99defede525e48ffec76f6cb2f74eda6f09d3575
Steven Speek <slspeek@gmail.com>
2013-04-11 16:14:19 +0200

    haskell platform install

--- install_haskell.sh
+++ install_haskell.sh
@@ -6,9 +6,10 @@ GHC=ghc-7.4.2-x86_64-unknown-linux.tar.bz2
 wget http://www.haskell.org/ghc/dist/7.4.2/$GHC
 tar xvjf $GHC
 cd ~/ghc-7.4.2
-./configure --prefix=~/haskell_tools
+./configure --prefix=$HOME/haskell_tools
 make install
+export PATH=$HOME/haskell_tools/bin:$PATH
 cd ~
 cd haskell-platform-2014.4.0.0
-./configure --prefix=~/haskell_tools
+./configure --prefix=$HOME/haskell_tools
 make install
--- run_as_root.sh
+++ run_as_root.sh
@@ -1,2 +1,2 @@
 apt-get update
-apt-get install bzip2 build-essential libgmp3-dev
+apt-get install bzip2 build-essential libgmp3-dev mesa-common-dev zlib1g-dev glutg3-dev
