efcb8135f6eee34b3a0ab868b5ab173726ffadb6
Steven Speek <slspeek@gmail.com>
2013-04-11 14:25:33 +0200

    installation build steps

--- install_haskell.sh
+++ install_haskell.sh
@@ -5,3 +5,10 @@ tar xvzf $HASKELL_PLATFORM
 GHC=ghc-7.4.2-x86_64-unknown-linux.tar.bz2
 wget http://www.haskell.org/ghc/dist/7.4.2/$GHC
 tar xvjf $GHC
+cd ~/ghc-7.4.2
+./configure --prefix=~/haskell_tools
+make install
+cd ~
+cd haskell-platform-2014.4.0.0
+./configure --prefix=~/haskell_tools
+make install
