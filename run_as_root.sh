a46567053e9e075508849a082dcb7b977e51328d
Steven Speek <slspeek@gmail.com>
2013-04-16 12:15:12 +0200

    added cairo dependency

--- run_as_root.sh
+++ run_as_root.sh
@@ -1,2 +1,3 @@
 apt-get update
-apt-get install bzip2 build-essential libgmp3-dev mesa-common-dev zlib1g-dev glutg3-dev
+apt-get install bzip2 build-essential libgmp3-dev mesa-common-dev zlib1g-dev glutg3-dev libcairo2-dev 
+
