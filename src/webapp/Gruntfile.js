3d61095eefeda4d9d6536e65b695389ecd016b1a
Steven L. Speek <slspeek@gmail.com>
2013-05-29 20:56:57 +0200

    Gruntfile lints

--- src/webapp/Gruntfile.js
+++ src/webapp/Gruntfile.js
@@ -76,6 +76,28 @@ module.exports = function (grunt) {
         url: 'http://localhost:<%= connect.options.port %>'
       }
     },
+
+    jsbeautifier: {
+      files : ['src/**/*.js'],
+      options : {
+        indent_size: 2,
+        indent_char: ' ',
+        indent_level: 0,
+        indent_with_tabs: false,
+        preserve_newlines: true,
+        max_preserve_newlines: 10,
+        jslint_happy: false,
+        brace_style: 'collapse',
+        keep_array_indentation: false,
+        keep_function_indentation: false,
+        space_before_conditional: true,
+        eval_code: false,
+        indent_case: false,
+        wrap_line_length: 80,
+        unescape_strings: false
+      }
+    },
+
     clean: {
       dist: {
         files: [{
---
+++ src/webapp/app/scripts/directives/directive.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* Directives */
+var app = angular.module('iconApp');
+
+// Add a directive for the bootstrap color picker widget
+// http://www.eyecon.ro/bootstrap-colorpicker/
+app.directive('colorpicker', function($log) {
+
+        return {
+            require: '?ngModel',
+            link: function(scope, element, attrs, controller) {
+                var updateModel;
+
+                if (controller != null) {
+                    updateModel = function(value) {
+                        return scope.$apply(function() {
+                                return controller.$setViewValue(value);
+                            });
+                    };
+
+                    controller.$render = function() {
+                        return element.colorpicker({
+                                color: controller.$viewValue,
+                                select: function(e, color) {
+                                    if (updateModel) {
+                                        $log.info(e);
+                                        updateModel(color.formatted);
+                                    }
+                                }
+                            });
+                    };
+                }
+
+                return element.colorpicker({}).on('changeColor', function(e) {
+                        if (updateModel) {
+                            updateModel(e.color.toHex());
+                        }
+                    });
+
+            }
+        };
+    });
