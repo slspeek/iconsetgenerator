b1a653a392788c67072698cbd172f51c42234d64
Steven L. Speek <slspeek@gmail.com>
2013-06-04 23:51:31 +0200

    Bookmarkable icons

--- src/webapp/app/scripts/app.js
+++ src/webapp/app/scripts/app.js
@@ -3,11 +3,11 @@
 angular.module('iconApp', ['ngResource','ui.bootstrap', 'ui.keypress'])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/designer/:IconName/:MainColor/:BgColor/:LineColor/:Background/:Shadow', {
+      .when('/:IconName/:MainColor/:BgColor/:LineColor/:Shadow/:Background', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
       .otherwise({
-        redirectTo: 'designer/overview/0077FD/F011DD/A20099/true/true'
+        redirectTo: 'overview/0077FD/F011DD/A20099/true/true'
       });
   });
--- src/webapp/app/scripts/controllers/main.js
+++ src/webapp/app/scripts/controllers/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var combinedWatch = function($scope) {
   return $scope.onBackground +
          $scope.shadow +
@@ -16,7 +17,7 @@ var iconParams = function($scope) {
       maincolor: $scope.mainColor,
       bgcolor: $scope.bgColor,
       linecolor: $scope.lineColor,
-      iconname: $.trim($scope.iconName)
+      iconname: $scope.iconName
     };
   if ($scope.shadow) {
     params.shadow = true;
@@ -27,39 +28,50 @@ var iconParams = function($scope) {
   return params;
 };
 
-var isTrue = function(arg) {
-  if (arg) {
-    return true;
-  } else {
-  	return false;
-  }
-}
+var iconHistoryToken = function($scope) {
+	var token = $scope.iconName + '/' +
+							$scope.mainColor + '/' +
+							$scope.bgColor + '/' +
+							$scope.lineColor + '/' +
+							$scope.shadow + '/' +
+							$scope.onBackground  ;
+	return token;
+};
+
+var updateLocation = function($scope, $location) {
+		if ($scope.mainColor !== undefined && $scope.bgColor !== undefined &&
+			$scope.lineColor !== undefined) {
+			$location.url(iconHistoryToken($scope));
+	  }
+  };
+
+var strToBoolean = function(str) {
+  if (str == 'false') {
+	  return false;
+		} else {
+		return true;
+		}
+};
 angular.module('iconApp')
   .controller('MainCtrl', function ($scope, $routeParams, $location, IconNames , DrawIcon, $log) {
     $scope.iconList = IconNames.list();
-    $scope.shadow = isTrue($routeParams.Shadow);
-    $scope.onBackground = isTrue($routeParams.Background);
+    $scope.shadow = strToBoolean($routeParams.Shadow);
+    $scope.onBackground = strToBoolean($routeParams.Background);
     $scope.mainColor = $routeParams.MainColor;
     $scope.bgColor = $routeParams.BgColor;
     $scope.lineColor = $routeParams.LineColor;
-	$scope.iconName = $routeParams.IconName;
+		$scope.iconName = $routeParams.IconName;
     $scope.iconUrl = '';
     $scope.pressS = function() {
       $log.info('Changed something');
     };
+		$scope.saveLocation = function() {
+			updateLocation($scope, $location);
+	  };
     $scope.$watch(combinedWatch, function() {
       $log.info('Watching works!');
       $scope.iconUrl = DrawIcon.get(iconParams($scope));
-		if ($scope.mainColor !== undefined && $scope.bgColor !== undefined &&
-			$scope.lineColor !== undefined) {
-			$location.url('designer/' + $scope.iconName + '/' +
-						  $scope.mainColor + '/' +
-						  $scope.bgColor + '/' +
-						  $scope.lineColor + '/' +
-						  $scope.shadow + '/' +
-						  $scope.onBackground);
-	    }
-    });
+		});
     $scope.setLineEqualToMainColor = function() {
       $scope.lineColor = $scope.mainColor;
       $log.info('line: ' + $scope.lineColor + ' main: ' + $scope.mainColor);
